e08b03505ee0b3fd6d93111293c2d0a53abb7e7a
itryse <itrysekr97@gmail.com>
2021-06-09 19:58:23 +0900

    検索ボックスの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   root to: 'posts#index'
+  get 'posts/search'
 
   resources :posts, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
+    collection do
+      get 'search'
+    end
   end
 end
