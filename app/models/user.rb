9a852ff88ac7d46389ae67a39be5eb7568488e52
itryse <itrysekr97@gmail.com>
2021-06-10 18:00:30 +0900

    READEMEの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments
+  
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
   def already_liked?(post)
